f7442e927dd5de158c7d289b6fd4399e20d991ce
bacfrb15 <bacfrb15@htlkaindorf.at>
2018-09-23 15:18:44 +0200

    Implementierung der WetterWert Klasse

--- Repetition_002/src/ex002/WetterWert.java
+++ Repetition_002/src/ex002/WetterWert.java
@@ -1,10 +1,47 @@
 package ex002;
 
+import java.util.Date;
+
 /**
  *
  * @author franz
  */
 public class WetterWert 
 {
+    private int temperatur;
+    private int luftfeuchtigkeit;
+    private Date zeitpunkt;
+
+    public WetterWert(int temperatur, int luftfeuchtigkeit, Date zeitpunkt) {
+        this.temperatur = temperatur;
+        this.luftfeuchtigkeit = luftfeuchtigkeit;
+        this.zeitpunkt = zeitpunkt;
+    }
+
+    public int getTemperatur() {
+        return temperatur;
+    }
+
+    public void setTemperatur(int temperatur) {
+        this.temperatur = temperatur;
+    }
+
+    public int getLuftfeuchtigkeit() {
+        return luftfeuchtigkeit;
+    }
+
+    public void setLuftfeuchtigkeit(int luftfeuchtigkeit) {
+        this.luftfeuchtigkeit = luftfeuchtigkeit;
+    }
+
+    public Date getZeitpunkt() {
+        return zeitpunkt;
+    }
+
+    public void setZeitpunkt(Date zeitpunkt) {
+        this.zeitpunkt = zeitpunkt;
+    }
+    
+    
     
 }
